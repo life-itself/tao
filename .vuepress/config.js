756f803d48d69e8e5dfca169d8fee87994574488
Rufus Pollock <rufus.pollock@datopian.com>
2020-01-25 22:47:09 +0100

    [*][m]: stub a skeleton vuepress site.
    * note i gitignore yarn.lock (and package-lock.json) b/c this isn't a dev project and often leads to unecessary confusion when it changes due to different people installing

---
+++ .vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: 'Art Earth Tech Playbook',
+  description: 'A guide to our culture and processes',
+  dest: 'public',
+  markdown: {
+    linkify: true,
+    typographer: true,
+    breaks: true,
+    html: true,
+    toc: {
+      includeLevel: [2, 3]
+    },
+    extendMarkdown: md => {
+      md.use(require('markdown-it-footnote'))
+    }
+  },
+  themeConfig: {
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'https://gitlab.com/artearthtech/playbook',
+    repoLabel: 'Contribute!',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    sidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/' },
+    ]
+  },
+  plugins: [
+    [
+      '@vuepress/plugin-google-analytics',
+      {
+        'ga': ''
+      }
+    ]
+  ]
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroText: Our Playbook
+actionText: Get Started →
+actionLink: /#welcome
+heroImage: https://artearthtech.com/images/aet-logo-02.svg
+footer: Copyright © 2016-present Art / Earth / Tech
+---
+
+# Welcome
+
+Welcome to the Art Earth Tech community.
+
